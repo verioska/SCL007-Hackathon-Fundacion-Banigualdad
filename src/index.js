8cc14f07c6a0c06b9586965ec0b38e9a0e689c3e
verioska <rverioska01@gmail.com>
2019-03-19 23:01:48 -0300

    pantalla 1 lista modo responsive

---
+++ src/index.js
@@ -0,0 +1,39 @@
+
+
+document.getElementById("personal").addEventListener("click", function(){
+   
+    document.getElementById('pantalla1').innerHTML = `
+
+    <img id="fotoContacto" src="Ellipse.png">
+    <p id="nombreContacto">Marian Alicia</p>
+    <p id="hora">12:30</p>
+    <img id="info" src="Ellipse2.png">
+    <img id="uno" src="1.png">
+    <hr id="linea">
+    <img id="fotoContacto" src="fabiola.png">
+    <p id="nombreContacto">Fabiola Barra</p>
+    <p id="hora">8:30</p>
+    <hr id="linea2">
+    <img id="fotoContacto" src="cecilia.png">
+    <p id="nombreContacto">Cecilia Valenzuela</p>
+    <p id="hora">18:11</p>
+    <hr id="linea2">
+    <img id="fotoContacto" src="gloria.png">
+    <p id="nombreContacto">Gloria Naranjo</p>
+    <p id="hora">16:30</p>
+    <hr id="linea2">
+    
+    `
+  })
+
+  
+document.getElementById("grupo").addEventListener("click", function(){
+   
+    alert("holas")
+  })
+
+  
+document.getElementById("eventos").addEventListener("click", function(){
+   
+    alert("holas")
+  })
